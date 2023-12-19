01fd2e17ae5147385a66de892b707387f03ec4a7
Jaynimi <Eyeslikejays@yahoo.com>
2023-12-19 10:11:52 +0100

    A start

---
+++ jaystodo/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [, set] = useState([]);
+}
+
+export default App;
---
+++ jaystodo/src/components/List.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { faEdit, FaTrash } from "react-icons/fa";
+const List = () => {
+  return <h2>List Component</h2>;
+};
+
+export default List;
