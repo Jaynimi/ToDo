699a5f620d92ceb41c9dc12c95d59550d9c6a478
Jaynimi <Eyeslikejays@yahoo.com>
2023-12-20 12:37:15 +0100

    Display functionality

--- jaystodo/src/App.jsx
+++ jaystodo/src/App.jsx
@@ -1,11 +1,55 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
+import List from "./components/List";
 
 function App() {
   const [name, setName] = useState("");
-  const [, set] = useState([]);
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  // const [, set] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  return (
+    <section className="section-center">
+      <form action="" onSubmit={handleSubmit} className="grocery-form">
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="fom-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default App;
--- jaystodo/src/components/List.jsx
+++ jaystodo/src/components/List.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-import { faEdit, FaTrash } from "react-icons/fa";
-const List = () => {
-  return <h2>List Component</h2>;
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { FaRegEdit, FaTrash } from "react-icons/fa";
+
+const List = ({ items }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article className="grocery-item" key={id}>
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button className="edit-btn" type="button">
+                <FaRegEdit />
+              </button>
+              <button className="delete-btn" type="button">
+                <RiDeleteBin6Line />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default List;
